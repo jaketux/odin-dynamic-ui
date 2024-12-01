9bf6b105b3f3e566d203be48f5ead3343093f3ed
jaketux <169253660+jaketux@users.noreply.github.com>
2024-12-02 08:29:09 +1100

    Image carousel Started working on the Image Carousel. My next step is to implement the JS so that it rotates.

---
+++ image-carousel/index.js
@@ -0,0 +1,8 @@
+const navLeft = document.querySelector(".nav-left")
+const navRight = document.querySelector(".nav-right")
+
+navLeft.addEventListener('click', function(){
+    const carouselImages = document.querySelectorAll('.carousel-img')
+    const navigationDots = document.querySelectorAll('.navigation-dot')
+})
+
