4dc72fd997ae1ff65cac613d1e48131d4210a070
jaketux <169253660+jaketux@users.noreply.github.com>
2024-12-02 22:17:31 +1100

    Image carousel
    I have created the image carousel and it is automatically moving, however the code is quite spaghetti with repeated code.
    
    I need to revisit this at a later stage to see if there's a way I can reduce this repetition.

--- image-carousel/index.js
+++ image-carousel/index.js
@@ -1,8 +1,83 @@
 const navLeft = document.querySelector(".nav-left")
 const navRight = document.querySelector(".nav-right")
+const carouselImages = document.querySelectorAll('.carousel-img')
+const navigationDots = document.querySelectorAll('.navigation-dot')
 
-navLeft.addEventListener('click', function(){
+function moveImagesLeft(){
     const carouselImages = document.querySelectorAll('.carousel-img')
     const navigationDots = document.querySelectorAll('.navigation-dot')
+    if (carouselImages[0].classList.contains('active-img')){
+        carouselImages[0].classList.remove('active-img')
+        carouselImages[0].classList.add('hidden')
+        carouselImages[3].classList.remove('hidden')
+        carouselImages[3].classList.add('active-img')
+        navigationDots[0].classList.remove('active-dot')
+        navigationDots[3].classList.add('active-dot')
+    } else if (carouselImages[1].classList.contains('active-img')){
+        carouselImages[1].classList.remove('active-img')
+        carouselImages[1].classList.add('hidden')
+        carouselImages[0].classList.remove('hidden')
+        carouselImages[0].classList.add('active-img')
+        navigationDots[1].classList.remove('active-dot')
+        navigationDots[0].classList.add('active-dot')
+    } else if (carouselImages[2].classList.contains('active-img')){
+        carouselImages[2].classList.remove('active-img')
+        carouselImages[2].classList.add('hidden')
+        carouselImages[1].classList.remove('hidden')
+        carouselImages[1].classList.add('active-img')
+        navigationDots[2].classList.remove('active-dot')
+        navigationDots[1].classList.add('active-dot')
+    } else if (carouselImages[3].classList.contains('active-img')){
+        carouselImages[3].classList.remove('active-img')
+        carouselImages[3].classList.add('hidden')
+        carouselImages[2].classList.remove('hidden')
+        carouselImages[2].classList.add('active-img')
+        navigationDots[3].classList.remove('active-dot')
+        navigationDots[2].classList.add('active-dot')
+    } 
+}
+
+function moveImagesRight(){
+    const carouselImages = document.querySelectorAll('.carousel-img')
+    const navigationDots = document.querySelectorAll('.navigation-dot')
+    if (carouselImages[0].classList.contains('active-img')){
+        carouselImages[0].classList.remove('active-img')
+        carouselImages[0].classList.add('hidden')
+        carouselImages[1].classList.remove('hidden')
+        carouselImages[1].classList.add('active-img')
+        navigationDots[0].classList.remove('active-dot')
+        navigationDots[1].classList.add('active-dot')
+    } else if (carouselImages[1].classList.contains('active-img')){
+        carouselImages[1].classList.remove('active-img')
+        carouselImages[1].classList.add('hidden')
+        carouselImages[2].classList.remove('hidden')
+        carouselImages[2].classList.add('active-img')
+        navigationDots[1].classList.remove('active-dot')
+        navigationDots[2].classList.add('active-dot')
+    } else if (carouselImages[2].classList.contains('active-img')){
+        carouselImages[2].classList.remove('active-img')
+        carouselImages[2].classList.add('hidden')
+        carouselImages[3].classList.remove('hidden')
+        carouselImages[3].classList.add('active-img')
+        navigationDots[2].classList.remove('active-dot')
+        navigationDots[3].classList.add('active-dot')
+    } else if (carouselImages[3].classList.contains('active-img')){
+        carouselImages[3].classList.remove('active-img')
+        carouselImages[3].classList.add('hidden')
+        carouselImages[0].classList.remove('hidden')
+        carouselImages[0].classList.add('active-img')
+        navigationDots[3].classList.remove('active-dot')
+        navigationDots[0].classList.add('active-dot')
+    } 
+}
+
+navLeft.addEventListener('click', function(){
+    moveImagesLeft()
 })
 
+navRight.addEventListener('click', function(){
+    moveImagesRight()
+
+})
+
+setInterval(moveImagesRight,3000)
